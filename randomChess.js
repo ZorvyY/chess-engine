5a5d840dc4213eca085d2eff98efa1b04162dfc4
Zoravur <zoravur.s@gmail.com>
2017-12-10 12:32:48 -0500

    Started chess lol

---
+++ randomChess.js
@@ -0,0 +1,18 @@
+const Chess = require('chess.js').Chess;
+var chess = new Chess();
+const Engine = require('node-uci').Engine
+
+const engine = new Engine('/home/zorvyyy/Documents/CompSci/Web/nodejs/piping/chess/stockfish')
+//for (var i = 0; i < 20; i++) {
+var x = 1;
+engine.chain()
+    .init()
+        .position(chess.fen())
+    engine.chain().init().go({depth: 15})
+    .then(result => {
+        chess.move({from: result.bestmove.slice(0, 2), to: result.bestmove.slice(2)});
+        console.log(chess.pgn());
+    })
+
+//}
+console.log(chess.pgn());
